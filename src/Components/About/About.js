be0f4563c999fa0f476fc2c82e30fd90192315c6
Bossman8 <jones.boston8@gmail.com>
2020-02-16 18:28:21 -0700

    updated pics

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,32 +3,32 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div className="about center">
+    <div className="about">
       <br></br>
-      <h3 style={{ color: "white" }}>Our Work!</h3>
+      <h3 className="center" style={{ color: "white" }}>
+        Our Work!
+      </h3>
       <br></br>
       <br></br>
       <br></br>
       <br></br>
-      <div className="row">
-        <div className="col s4 offset-s6" style={{ color: "white" }}></div>
-      </div>
-      <div className="container">
+
+      <div className="container" id="pics">
         <img
-          classname="materialboxed"
+          class="materialboxed"
           id="aboutPic"
           src="https://i.imgur.com/1z4oEA6.jpg"
-        />
+        ></img>
         <img
-          classname="materialboxed"
+          class="materialboxed"
+          id="aboutPic"
           src="https://i.imgur.com/f3osdc6.jpg"
-          id="aboutPic"
-        />
+        ></img>
         <img
-          classname="materialboxed"
-          src="https://i.imgur.com/1l9Ks9g.jpg"
+          class="materialboxed"
           id="aboutPic"
-        />
+          src="https://i.imgur.com/1l9Ks9g.jpg"
+        ></img>
       </div>
     </div>
   );
